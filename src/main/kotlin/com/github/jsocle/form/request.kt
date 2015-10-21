df20b71a1ec957ca52e3755c0ef6bfcd2e68add8
vamf12 <vamf12@gmail.com>
2015-10-21 18:31:04 +0900

    remove request setter internal modifier

--- src/main/kotlin/com/github/jsocle/form/request.kt
+++ src/main/kotlin/com/github/jsocle/form/request.kt
@@ -4,10 +4,8 @@ object request {
     var parameters: () -> Map<String, Array<String>> = {
         throw IllegalArgumentException("com.github.jsocle.form.request parameters was not set.")
     }
-        internal set
 
     var method: () -> String = {
         throw IllegalArgumentException("com.github.jsocle.form.request method was not set.")
     }
-        internal set
 }
